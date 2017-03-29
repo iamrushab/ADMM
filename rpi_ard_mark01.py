ec4d3aa41211a5db2ca76a70f9601390d183ef7b
Rushabh <rushabhpatel100@gmail.com>
2017-03-29 11:51:51 +0000

    Update rpi_ard_mark01.py
    Added remaning two buttons

--- rpi_ard_mark01.py
+++ rpi_ard_mark01.py
@@ -1,13 +1,20 @@
 import serial
 from Tkinter import *
-arduinoSerialData= serial.Serial('/dev/ttyACM0',9600)
+ard= serial.Serial('/dev/ttyACM0',9600)
 root = Tk()
+
 def coke():
-    arduinoSerialData.write('0,5,0')
-    print arduinoSerialData.('0,5,0')
+    ard.write('0,1,0')
+    
 
 def sprite():
-    arduinoSerialData.write('0,6,0')
+    ard.write('0,2,0')
+
+def fanta():
+    ard.write('0,3,0')
+
+def tu():
+    ard.write('0,4,0')
 
 top=Label(root)
 top.pack()
@@ -16,10 +23,13 @@ bottomFrame.pack(side=BOTTOM)
 
 button1= Button(top, text="CocoCola", command=coke)
 button2= Button(top, text="Sprite", command=sprite)
+button3= Button(top, text="Fanta", command=fanta)
+button4= Button(top, text="Thumbs Up", command=tu)
 button1.pack(side=LEFT)
 button2.pack(side=RIGHT)
-
-
+button3.pack(side=RIGHT)
+button4.pack(side=RIGHT)
 
 
 root.mainloop()
+
