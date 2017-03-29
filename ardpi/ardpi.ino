f8a545a344dfe4dc5597fdc80f7170ab89a266ff
Rushabh <rushabhpatel100@gmail.com>
2017-03-29 11:59:45 +0000

    Update ardpi.ino
    Update code for all 6 reed switches

--- ardpi/ardpi.ino
+++ ardpi/ardpi.ino
@@ -1,104 +1,61 @@
- int r1= 4;
- int rr;
- int rt=0;
- int rf;
- int ry;
+ int lr= 0;
+ int rr=1;
+ int cr=2;
+ int tr=3;
+ int fr=4;
+ int sr=5;
+ 
+ int l;
  int r;
- int t=2500;
-
-#include <Stepper.h>
+ int c;
+ int t;
+ int f;
+ int s;
+ #include <Stepper.h>
 const int stepsPerRevolution = 50; 
 Stepper step1(stepsPerRevolution, 8, 9, 10, 11);
 //Stepper step2(stepsPerRevolution, 2, 3, 4, 5);
 int stepCount=0;
 int n;
 
-void setup()
-{
-  pinMode(r1, INPUT);
+
+void setup(){
   Serial.begin(9600);
-  attachInterrupt(rf, stopm, 1);
-  
+  pinMode(lr, INPUT);
 }
-again1:
-void loop()
-{
+void loop(){
+   if(l!=0){
+    step1.setSpeed(80);
+    step1.step(-10);
+  }
   
-  delay(700);
   if(Serial.available())
   {
     n=Serial.read()-'0';
   }
   delay(70);
-  rr=digitalRead(r1);
-  if(rr==1)
-  {
-    rt = millis();
-  }
-  else if(rr==0){
-    ry = millis();}
-  //Serial.println(rr);
-  //Serial.println(rt);
-  if(rt-t>6000 && rt-ry>3000){
-    rf=1;
-   // if(rf=1){
-     // t=millis();
-    //}
-    }
-  else{
-    rf=0;
-  }
-  
-  Serial.println(rf);
-  //Serial.println("1: ");
-  //Serial.println(rt);
-  //Serial.println("0: ");
-  //Serial.println(ry);
-  
-  //Serial.println(n);
-  //delay(10);
-
+  l=pulseIn(lr, HIGH);
   switch(n)
   {
-   case 5:
-   //digitalWrite(led,HIGH);
-   step1.setSpeed(80);
-   step1.step(-600); 
-   //step2.setSpeed(80);
-   //step2.step(-200);
-   // if(rr==0)
-  //{
-    //step1.setSpeed(0);
-    //step1.step(0);
-  //} 
-   break;
+     case 1:
+     step1.setSpeed(80);
+     step1.step(1000);
+     break;
    
-   //case 0:
-   //digitalWrite(led,LOW);
-   //break;
+    case 2:
+    step1.setSpeed(80);
+    step1.step(1500);
+    break;  
    
-   case 6:
-   step1.setSpeed(80);
-   //step2.setSpeed(80);
-   step1.step(600);
-    // if(rr==0)
-  //{
-    //step1.setSpeed(0);
-    //step1.step(0);
-  //} 
-   //step2.step(200);
-   //step2.step(200);
-   break;
-}
-  
-}
+    case 3:
+     step1.setSpeed(80);
+     step1.step(1000);
+    break;
+   
+   case 4:
+    step1.setSpeed(80);
+    step1.step(1500);
+   break;    
+  }
 
-void stopm(){
-  step1.setSpeed(0);
-  step1.step(0);
-  delay(500);
-  step1.setSpeed(20);
-  step1.step(50);
-  goto again1;
-  
 }
